f2c422d25b47a2b7a7e3d220915e54628b724f2d
alalalalaki <harlan.zhu@gmail.com>
2023-05-20 02:12:30 +0900

    Add note to pd_perform

--- autil/pd_perform.py
+++ autil/pd_perform.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 
 def chunk_apply(df, func, args=(), n=10_000):
 
+    # alternative maybe can try this package: https://github.com/nalepae/pandarallel
+
     m = df.shape[0]//n + 1
     list_dfjb = (df[i:i+n] for i in range(0, df.shape[0], n))
 
